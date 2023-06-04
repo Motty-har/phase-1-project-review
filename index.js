577c5d930b1b2dd02ce1704f4be3b2b66f9f047a
Motty-har <harligmotty@gmail.com>
2023-06-04 10:40:58 -0400

    in event listener created empty object to hold value of submit form

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const submitForm = document.getElementById('post')
 addEventListener("DOMContentLoaded", submitForm.addEventListener("click", (event) => {
     event.preventDefault()
+    const firstName = event.target[0].value
+    const lastName = event.target[1].value
+    const goals = event.target[2].value
+    const assists = event.target[3].value
+    const playerStats = {
+        firstname: firstName,
+        lastname: lastName,
+        goals: goals,
+        assists: assists
+}
 }))
